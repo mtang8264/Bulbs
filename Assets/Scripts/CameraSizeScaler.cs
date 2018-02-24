5a91547c5e0e822559669d8fb2b6e15e0cedbcec
Michael Tang <mtang8264@gmail.com>
2018-02-23 21:10:10 -0500

    1.0.5
    Fixed screen resolution for mobile
    Added scenes for PC only releases
    Fixed stat screen bug where everything went away

---
+++ Assets/Scripts/CameraSizeScaler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSizeScaler : MonoBehaviour {
+	Camera cam;
+
+	// Use this for initialization
+	void Start () {
+		cam = GetComponent<Camera> ();
+		cam.orthographicSize = 2.81109445f / Screen.width * Screen.height;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
